8b57333d0aa932aff224bb9e196a72f801d9edce
905585272 <905585272@qq.com>
2020-11-17 20:13:28 +0800

    enterPage

--- src/components/enterPage.vue
+++ src/components/enterPage.vue
@@ -1,25 +1,30 @@
 <template>
   <div class="container text-center">
-      <h1>登录界面</h1>
-      <form class="form-group">
-            <label for="">&nbsp;&nbsp;&nbsp;<span class="xinghao">*</span>系统ID：</label><input type="text" required class="input" maxlength="30"> <br/>
-            <label for=""><span class="xinghao">*</span>输入密码：</label><input type="password" required class="input" maxlength="30"> <br/>
-            <input type="submit" value="登录">
-        </form>
+    <h1>登录界面</h1>
+    <form class="form-group">
+      <div class="col-md-6">
+        <label for="systemId">系统ID：</label>
+        <input type="text" required maxlength="30" id="systemId" />
+      </div>
+      <div class="col-md-6">
+        <label for="password">输入密码：</label>
+        <input type="password" required maxlength="30" id="password" />
+      </div>
+      <div class="col-md-6">
+        <input type="submit" value="登录" />
+      </div>
+    </form>
   </div>
 </template>
 
 <script>
 export default {
-  data(){
-    return{
-
-    }
-  }
+  data() {
+    return {};
+  },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
- 
 </style>
--- src/components/registerPage.vue
+++ src/components/registerPage.vue
@@ -1,72 +1,71 @@
 <template>
-<div class="container">
-  <div class="text-center jumbotron">
+  <div class="container">
+    <div class="text-center jumbotron">
       <h1>注册界面</h1>
-  </div>
-  <div class="content text-center">
+      <h2>123</h2>
+    </div>
+    <div class="content text-center">
       <form class="form-group">
-          <div class="col-md-6">
+        <div class="col-md-6">
           <label for="id">登录ID：</label>
-          <input type="text" v-model="id" maxlength="30">
-          </div>
-          <div class="col-md-6">
+          <input type="text" v-model="id" maxlength="30" />
+        </div>
+        <div class="col-md-6">
           <label for="name">*姓名：</label>
-          <input  type="text" v-model="name" maxlength="20">
-          </div>
-          <div class="col-md-6">
+          <input type="text" v-model="name" maxlength="20" />
+        </div>
+        <div class="col-md-6">
           <label for="email">*邮箱：</label>
-          <input type="text" v-model="email" maxlength="30">
-          </div>
-          <div class="col-md-6">
+          <input type="text" v-model="email" maxlength="30" />
+        </div>
+        <div class="col-md-6">
           <label for="password">*输入密码：</label>
-          <input type="text" v-model="passwordOne" maxlength="30">
-          </div>
-          <div class="col-md-6">
+          <input type="text" v-model="passwordOne" maxlength="30" />
+        </div>
+        <div class="col-md-6">
           <label for="password">*确认密码：</label>
-          <input type="text" v-model="passwordTwo" maxlength="30">
-          </div>
-          <input type="submit" class="form-control" @click="submit">
+          <input type="text" v-model="passwordTwo" maxlength="30" />
+        </div>
+        <input type="submit" class="form-control" @click="submit" />
       </form>
-  </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  data(){
-    return{
-        id:'',
-        name:'',
-        email:'',
-        passwordOne:'',
-        passwordTwo:''
-    }
+  data() {
+    return {
+      id: "",
+      name: "",
+      email: "",
+      passwordOne: "",
+      passwordTwo: "",
+    };
   },
   methods: {
-      submit:function(){
-          if(this.passwordOne == this.passwordTwo){
-              alert('密码正确！');
-          }
-          else if(this.passwordOne != this.passwordTwo){
-              alert('密码错误！');
-          }
+    submit: function () {
+      if (this.passwordOne == this.passwordTwo) {
+        alert("密码正确！");
+      } else if (this.passwordOne != this.passwordTwo) {
+        alert("密码错误！");
       }
+    },
   },
-//    mounted () {
-//     this.$axios
-//       .get('')
-//       .then()
-//       .catch(function (error) { // 请求失败处理
-//         console.log(error);
-//       });
-//   }
+  //    mounted () {
+  //     this.$axios
+  //       .get('')
+  //       .then()
+  //       .catch(function (error) { // 请求失败处理
+  //         console.log(error);
+  //       });
+  //   }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
- .content{
-     width: 500px;
-     
- }
+.content {
+  width: 500px;
+}
 </style>
