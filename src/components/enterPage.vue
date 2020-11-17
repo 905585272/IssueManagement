0485abc8f193cc97313f130d5a1119edd276067a
905585272 <905585272@qq.com>
2020-11-17 15:30:39 +0800

    Update enterPage.vue

--- src/components/enterPage.vue
+++ src/components/enterPage.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="container text-center">
       <h1>登录界面</h1>
+      <form class="form-group">
+            <label for="">&nbsp;&nbsp;&nbsp;<span class="xinghao">*</span>系统ID：</label><input type="text" required class="input" maxlength="30"> <br/>
+            <label for=""><span class="xinghao">*</span>输入密码：</label><input type="password" required class="input" maxlength="30"> <br/>
+            <input type="submit" value="登录">
+        </form>
   </div>
 </template>
 
